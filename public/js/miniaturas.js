9db6147a17bfa91b3a9aa1c0b9800aa363671828
lucaspiriz97 <pirizlucas97@gmail.com>
2024-11-27 17:13:23 -0300

    agrego miniaturas y css

---
+++ public/js/miniaturas.js
@@ -0,0 +1,5 @@
+// Cambia la imagen principal al hacer clic en una miniatura
+function changeImage(imagePath) {
+    const mainImage = document.getElementById("mainImage");
+    mainImage.src = imagePath;
+}
